4879744250eb2c230a1a13dd2bc6b0b1b1c414e6
rian-van-den-ander <52826135+rian-van-den-ander@users.noreply.github.com>
2019-08-01 09:01:01 +0200

    Uploading code

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # encode_json_data_within_dataframe
 Encode json data within a dataframe
+
+Inputs:
+* pandas_data_frame: A pandas dataframe with a json column within it
+* json_column_index: index of that column
+* json_id_column: the unique identifier of the data you're trying to extract, e.g. "id" or "name"
+
+Example input:
+
+![JSON movie data](https://rian-van-den-ander.github.io/images/algo/encoding_json/input.png "Algorithm input")
+
+
+Example output:
+
+![JSON movie data](https://rian-van-den-ander.github.io/images/algo/encoding_json/output.png "Algorithm output")
+
+Example usage:
+
+~~~~
+
+dataset = pd.read_csv('tmdb_5000_movies.csv')
+dataset = dataset[["revenue","genres"]]
+dataset = dataset[0:2]
+
+encode_json_column(dataset,1,"id")
+
+~~~~
---
+++ encode_json_column.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 09:09:17 2019
+@author: rian-van-den-ander
+"""
+
+""" #example usage
+dataset = pd.read_csv('tmdb_5000_movies.csv')
+dataset = dataset[["revenue","genres"]]
+dataset = dataset[0:2]
+
+encode_json_column(dataset,1,"id")
+"""
+
+import pandas as pd
+import json
+
+def is_json(myjson):
+    try:
+        json_object = json.loads(myjson)
+    except:
+        return False
+    return True
+
+def encode_json_column(pandas_data_frame, json_column_index=0, json_id_column="id"):
+        
+    X = pandas_data_frame.iloc[:, :].values
+            
+    #keep track of whether a column has been encoded already, else we'd reset all the values to 0
+    encodedcolumns = []
+    count = 0
+    
+    #for each row in the data
+    for row in X:
+        
+        if(is_json(row[json_column_index])): #some data is just not json. ignore
+            
+            #for each feature in the json
+            for json_features in json.loads(row[json_column_index]):
+                
+                #pick out its id (the json identifier you specifc in json_id_column)
+                featureid = json_features[json_id_column]
+                
+                #if this id hasn't been seen yet, add it to the dataframe with default 0
+                if featureid not in encodedcolumns:
+                    encodedcolumns.append(featureid)
+                    pandas_data_frame[featureid]=0
+                   
+                #else just set it to 1 here
+                pandas_data_frame[featureid][count] = 1
+    
+        count+=1
+
+    #drop the original json column
+    pandas_data_frame = pandas_data_frame.drop(pandas_data_frame.columns[json_column_index], 1)
+    
+    return pandas_data_frame
+
+
